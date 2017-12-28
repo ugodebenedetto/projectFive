51dd6f323d26d558382744f35ffbe54768ebeb63
crescdav <30698783+crescdav@users.noreply.github.com>
2017-12-28 18:13:50 +0100

    Classi jUnit

---
+++ TirocinioSmart/test/it/tirociniosmart/control/utente/LogoutTest.java
@@ -0,0 +1,32 @@
+package it.tirociniosmart.control.utente;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class LogoutTest {
+
+  @Before
+  public void setUp() throws Exception {}
+
+  @After
+  public void tearDown() throws Exception {}
+
+  @Test
+  public void testDoGetHttpServletRequestHttpServletResponse() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testDoPostHttpServletRequestHttpServletResponse() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testLogout() {
+    fail("Not yet implemented");
+  }
+
+}
---
+++ TirocinioSmart/test/it/tirociniosmart/control/utente/RecuperaPasswordTest.java
@@ -0,0 +1,27 @@
+package it.tirociniosmart.control.utente;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class RecuperaPasswordTest {
+
+  @Before
+  public void setUp() throws Exception {}
+
+  @After
+  public void tearDown() throws Exception {}
+
+  @Test
+  public void testDoGetHttpServletRequestHttpServletResponse() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testDoPostHttpServletRequestHttpServletResponse() {
+    fail("Not yet implemented");
+  }
+
+}
