6aacdccff96e18ebe015897fa2c86af67810810b
ugodebenedetto <ugodebenedetto@mbp-di-ugo.station>
2018-01-01 11:15:21 +0100

    resa funzionante la servlet con i dati di esempio in mancanza del model

--- TirocinioSmart/src/it/tirociniosmart/control/didattica/insertandedit/ModificaAnnuncio.java
+++ TirocinioSmart/src/it/tirociniosmart/control/didattica/insertandedit/ModificaAnnuncio.java
@@ -9,14 +9,29 @@ package it.tirociniosmart.control.didattica.insertandedit;
 import it.tirociniosmart.model.annuncio.Annuncio;
 import it.tirociniosmart.model.annuncio.ProxyAnnuncioDao;
 import it.tirociniosmart.model.factory.FactoryProducer;
+import it.tirociniosmart.model.persistancetools.FileManager;
+import it.tirociniosmart.model.persistancetools.FileNotSupportedException;
+import it.tirociniosmart.model.utente.Didattica;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@MultipartConfig
+@WebServlet("/ModificaAnnuncio")
 public class ModificaAnnuncio extends HttpServlet {
 
   private static final long serialVersionUID = 1L;
+  private String filePosition = "ok";
+  private String url;
+  private boolean flag;
 
 
   /**
@@ -33,19 +48,60 @@ public class ModificaAnnuncio extends HttpServlet {
    * 
    * @param request richiesta che arriva alla servlet
    * @param response risposta della servlet
+   * @throws IOException eccezione
    */
 
-  public void doPost(HttpServletRequest request, HttpServletResponse response) {
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
-    // prendo l'annuncio che voglio modificare
+    flag = true;
+    url = "it.tirociniosmart.view.didattica/crea_annuncio_success.jsp";
+    String titolo = request.getParameter("nome");
+    String body = request.getParameter("body");
+    String ot = request.getParameter("title");
+    Date data = new Date();
+    Annuncio annuncio = new Annuncio();
+    // Didattica d = (Didattica) request.getSession().getAttribute("currentSessionUser");
+    Didattica d = new Didattica("", "", "", "", "", "", "", "", "", "", "", false);
+    ArrayList<Annuncio> annunci =
+        (ArrayList<Annuncio>) request.getSession().getAttribute("annunci");
+    for (Annuncio a : annunci) {
+      if (a.getTitolo().equalsIgnoreCase(ot)) {
+        annuncio = a;
+        annunci.remove(a);
+        break;
+      }
+    }
+    if (flag) {
+      annuncio.setAutore(d);
+      annuncio.setTitolo(titolo);
+      annuncio.setData(data.toString());
+      annuncio.setFilePosition(filePosition);
 
-    FactoryProducer factory = FactoryProducer.getIstance();
-    ProxyAnnuncioDao proxyAnnuncio = (ProxyAnnuncioDao) factory.getAnnuncioDao();
-    Annuncio annuncio = proxyAnnuncio.findAnnuncio(null);
-    // prendo i dati dalla form e creo un nuovo annuncio
+      FileManager filemanager = FileManager.getIstance();
+      try {
+        filemanager.saveFile(request, filePosition);
+        annunci.add(annuncio);
+      } catch (FileNotSupportedException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+      } catch (IOException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+      } catch (ServletException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+      }
+    }
 
-    modificaAnnuncio(null, null);
-
+    request.getSession().setAttribute("annunci", annunci);
+    response.sendRedirect(url);
+    /*
+     * FactoryProducer factory = FactoryProducer.getIstance(); ProxyAnnuncioDao proxyAnnuncio =
+     * (ProxyAnnuncioDao) factory.getAnnuncioDao(); Annuncio annuncio =
+     * proxyAnnuncio.findAnnuncio(null); // prendo i dati dalla form e creo un nuovo annuncio
+     * 
+     * modificaAnnuncio(null, null);
+     */
 
   }
 
